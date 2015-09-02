27a91ac0946fadcca991e331e7e814056df7d837
no-glue <nikolapav1985@gmail.com>
2015-09-02 05:11:21 +0200

    layout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 import dog.*;
 
 public class Main {
   public static void main(String args[]) {
-    get("/hello", (req, res) -> "Hello from Main.java");
+    get("/", (req, res) -> {
+      return new ModelAndView(new HashMap(), "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
   }
 }
