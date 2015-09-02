6339718020c6154b889ec2008178fb970f750469
no-glue <nikolapav1985@gmail.com>
2015-09-02 15:22:11 +0200

    form

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.LinkedList;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
@@ -7,14 +8,19 @@ import dog.*;
 public class Main {
   public static void main(String args[]) {
     String layout = "templates/layout.vtl";
+    LinkedList<Dog> breed = new LinkedList<Dog>();
     get("/", (req, res) -> {
       HashMap model = new HashMap();
       model.put("template", "templates/dogs.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
     get("/breed", (req, res) -> {
+      String _breed = req.queryParams("breed");
+      Dog dog = new Dog(_breed);
+      breed.add(dog);
       HashMap model = new HashMap();
-      model.put("template", "templates/dogs.vtl");
+      model.put("breed", breed);
+      model.put("template", "templates/breed.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
